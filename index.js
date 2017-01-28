68b8f5bd02924ab1cd04b6bad77a9880334a2a4f
Jacob Bond <jacobpbond@hotmail.co.uk>
2017-01-28 23:49:13 +0000

    Rework

--- index.js
+++ index.js
@@ -4,13 +4,65 @@ const client = new Discord.Client();
 const broadcast = client.createVoiceBroadcast();
 
 /* Queue */
-const d_queue = [
-  ["https://www.youtube.com/watch?v=RJOqJ-RitOg", "Steve Aoki & Louis Tomlinson - Just Hold On"],
-  ["https://www.youtube.com/watch?v=_dK2tDK9grQ", "Ed Sheeran - Shape of You"],
-  ["https://www.youtube.com/watch?v=papuvlVeZg8", "Clean Bandit - Rockabye"],
-  ["https://www.youtube.com/watch?v=RhU9MZ98jxo", "The Chainsmokers - Paris"]
-]
+const json = require(`./queue.json`).songs;
+const d_queue = json;
 let queue = d_queue;
+
+/* Commands */
+const commands = {
+  'ping': {
+    name: 'ping',
+    info: 'Checks the bots ping time.',
+    func: (message, args) => {
+      const now = new Date();
+      message.channel.sendMessage(`Ping?`).then(sent => {
+        sent.edit(`Pong! Took ${new Date() - now}ms.`);
+      });
+    }
+  },
+  'np': {
+    name: 'np',
+    info: 'Sees whats currently playing.',
+    func: (message, args) => {
+      message.channel.sendMessage(`:musical_note: Now playing **${queue[0][1]}**.`);
+    }
+  },
+  'join': {
+    name: 'join',
+    info: 'Joins your voice channel and starts streaming.',
+    func: (message, args) => {
+      if(message.member.voiceChannel) {
+        message.member.voiceChannel.join().then(vc => { vc.playBroadcast(broadcast); message.channel.sendMessage(':heavy_check_mark:'); })
+      } else {
+        message.channel.sendMessage(':exclamation: You need to be in a voice channel.');
+      }
+    }
+  },
+  'leave': {
+    name: 'leave',
+    info: 'Leavs your voice channel.',
+    func: (message, args) => {
+      if(message.member.voiceChannel) {
+        message.member.voiceChannel.leave();
+        message.channel.sendMessage(':heavy_check_mark:');
+      } else {
+        message.channel.sendMessage(':exclamation: You need to be in a voice channel.');
+      }
+    }
+  },
+  'help': {
+    name: 'help',
+    info: 'Shows commands for the bot.',
+    func: (message, args) => {
+      let final = ``;
+      for(const command in commands) {
+        final += `\n${commands[command].name}\n\t${commands[command].desc}`
+      }
+
+      message.channel.sendMessage(`__**Commands**__\n${final}`);
+    }
+  }
+}
 
 /* Run */
 function run() {
@@ -36,34 +88,17 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  const content = message.content.toLowerCase();
+  if(message.content.startsWith('a|')) {
+    let command = message.content.substring(2).split(" ")[0];
+    let args = message.content.substring(3 + command.length);
 
-  if(content.startsWith(`a|join`)) {
-    if(message.member.voiceChannel) {
-      message.member.voiceChannel.join().then(vc => { vc.playBroadcast(broadcast); message.channel.sendMessage(':heavy_check_mark:'); })
-    } else {
-      message.channel.sendMessage(':exclamation: You need to be in a voice channel.');
+    if(commands[command]) {
+      try {
+        commands[command].func(message, args);
+      } catch(e) {
+        console.error(e);
+      }
     }
-  }
-
-  if(content.startsWith(`a|leave`)) {
-    if(message.member.voiceChannel) {
-      message.member.voiceChannel.leave();
-      message.channel.sendMessage(':heavy_check_mark:');
-    } else {
-      message.channel.sendMessage(':exclamation: You need to be in a voice channel.');
-    }
-  }
-
-  if(content.startsWith(`a|np`)) {
-    message.channel.sendMessage(`:musical_note: Now playing **${queue[0][1]}**.`);
-  }
-
-  if(content.startsWith(`a|ping`)) {
-    const now = new Date();
-    message.channel.sendMessage(`Ping?`).then(sent => {
-      sent.edit(`Pong! Took ${new Date() - now}ms.`);
-    });
   }
 })
 
