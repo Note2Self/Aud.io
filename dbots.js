5d8aea6a75718ae60cf0d2008c2647f115518a8e
Jacob Bond <jacobpbond@hotmail.co.uk>
2017-01-29 00:31:01 +0000

    Add files via upload

--- dbots.js
+++ dbots.js
@@ -2,7 +2,7 @@ module.exports = (guilds) => {
   require('unirest').post('https://bots.discord.pw/api/bots/272419982619705346/stats')
   .headers({
     'Content-Type': 'application/json',
-    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiIxMTYyOTMwMTg3NDI1NTQ2MjUiLCJyYW5kIjo4OTgsImlhdCI6MTQ3MDg2MDU5OH0.ZLWQOD3BXq4BdpyXPxH8BfvOk5NWaedhsKiCilFbTxc'
+    'Authorization': require(`./config.json`).dbots
   })
   .send({'server_count': guilds})
   .end(res => {})
