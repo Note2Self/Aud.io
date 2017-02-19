e22593772361e6f3db8bf88d0dde2be008787bdd
Jacob Bond <jacobpbond@hotmail.co.uk>
2017-02-19 18:24:42 +0000

    Delete dbots.js

--- dbots.js
+++
@@ -1,9 +0,0 @@
-module.exports = (guilds) => {
-  require('unirest').post('https://bots.discord.pw/api/bots/272419982619705346/stats')
-  .headers({
-    'Content-Type': 'application/json',
-    'Authorization': require(`./config.json`).dbots
-  })
-  .send({'server_count': guilds})
-  .end(res => {})
-}
